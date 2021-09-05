bca0d5412368da33e07de4c30e47b021b360acab
thals1214 <thaxls@ewhain.net>
2021-09-05 18:15:38 +0900

    [정렬] 9월 5일

---
+++ 9월 3일 - 정렬/11651.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(pair<int, int> a, pair<int, int> b) {
+	if (a.second == b.second)
+		return a.first < b.first;
+	else
+		return a.second < b.second;
+}
+
+int main() {
+	int N;
+	cin >> N;
+
+	vector<pair<int, int>> arr;
+	arr.assign(N, {}); // initialize
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i].first >> arr[i].second;
+	}
+
+	sort(arr.begin(), arr.end(),cmp);
+	
+	for (int i = 0; i < N; i++) {
+		cout << arr[i].first << " " << arr[i].second << '\n';
+	}
+}
