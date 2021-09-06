57e887f89e42b255fde16e02a23bbe7293536685
thals1214 <thaxls@ewhain.net>
2021-09-06 20:30:05 +0900

    Revert "Update 11651.cpp"
    This reverts commit 34def7e1d550c049a40e17864b3ec405dcf41b8d.

--- 9월 3일 - 정렬/11651.cpp
+++ 9월 3일 - 정렬/11651.cpp
@@ -6,8 +6,9 @@ using namespace std;
 
 bool cmp(pair<int, int> a, pair<int, int> b) {
 	if (a.second == b.second)
-		return a.first < b.first;	
-	return a.second < b.second;
+		return a.first < b.first;
+	else
+		return a.second < b.second;
 }
 
 int main() {
