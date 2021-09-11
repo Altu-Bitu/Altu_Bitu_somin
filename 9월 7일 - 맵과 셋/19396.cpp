8d6066f7b9c5f77a986b8b74f48f7331ece5a423
thals1214 <thaxls@ewhain.net>
2021-09-11 18:23:41 +0900

    [맵과 셋] 9월 11일 -ing

---
+++ 9월 7일 - 맵과 셋/19396.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int w, l_0, T; // 다이어트 전 체중, 섭취량/대사량 , 기초대사량 변화 역치
+	int D, l, A; // 다이어트 기간, 에너지 섭취량, 활동 대사량
+
+	cin >> w >> l_0 >> T;
+	cin >> D >> l >> A;
+
+
+	// 출력
+	// 다이어트 후 예상 체중, 일일 기초 대사량
+}
