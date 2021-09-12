5e67adb8e217c1d51cd3309223b3a395fa1e4684
thals1214 <thaxls@ewhain.net>
2021-09-12 15:05:46 +0900

    [맵과 셋] 9월 12일 -ing

--- 9월 7일 - 맵과 셋/10757.cpp
+++ 9월 7일 - 맵과 셋/10757.cpp
@@ -3,49 +3,38 @@
 
 using namespace std;
 
-// ing
 int main() {
-	string a, b;
+	string A, B;
+	cin >> A >> B;
+
 	vector<int> a_arr, b_arr, result;
+	int max_size = max(A.size(), B.size());
 
-	// 입력
-	cin >> a >> b;
-	a_arr.assign(a.length(), 0);
-	b_arr.assign(b.length(), 0);
-	result.assign(max(a.length(), b.length()) + 1,0); // result size -> + 1
-
-	for (int i = 0; i < a.length(); i++) 
-		a_arr[i] = a[a.length()- 1- i] - '0'; 
-	for (int i = 0; i < b.length(); i++) 
-		b_arr[i] = b[b.length() - 1 - i] - '0';  
-	
-	int a_length = a.length() , b_length = b.length();
-	int i = 0; // iterative purpose
-
-	// 연산 
-	while (a_length > 0 && b_length > 0) {
-		result[i] += (a_arr[i] + b_arr[i]) % 10;
-		if ((a_arr[i] + b_arr[i]) / 10 > 0) result[i+1]++;
-		a_length--; b_length--;
-		i++;
+	/*input -> 1의자리부터 */
+	for (int i = 0; i < max_size; i++) {
+		if (i < A.size()) a_arr.push_back(A[A.size() - 1 - i] - '0');
+		else a_arr.push_back(0);
+		
 	}
-	// 남은 경우
-	while (a_length > 0) {
-		result[i] += a_arr[i];
-		if (a_arr[i] / 10 > 0) result[i+1]++;
-		a_length--; i++;
-	}
-	while (b_length > 0) {
-		result[i] += b_arr[i];
-		if (b_arr[i] / 10 > 0) result[i+1]++;
-		b_length--; i++;
+	for (int i = 0; i < max_size; i++) {
+		if (i < B.size()) b_arr.push_back(B[B.size() - 1 - i] - '0');
+		else b_arr.push_back(0);
 	}
 
-	// i 처리 해주기 
-	if (result[i] == 0) i--;
+	/*calculate*/
+	int size = 0;
+	result.assign(max(a_arr.size(), b_arr.size()) + 1, 0);
 
-	// 출력
-	for (int j = i; j >=0; j--) {
-		cout << result[j];
+	while (size < max(a_arr.size(),b_arr.size())){
+		result[size] += (a_arr[size] + b_arr[size]) % 10;
+		if ((a_arr[size] + b_arr[size]) / 10 > 0)
+			result[size+1]++;
+		size++;
 	}
+	if (result[size] == 0) size--; 
+
+	/* 거꾸로 출력 */
+	for (int i = size; i >= 0; i--)
+		cout << result[i];
+
 }
