814054e50041766fce0bc137d57393ddd8c30759
thals1214 <thaxls@ewhain.net>
2021-09-20 00:18:58 +0900

    [우선순위큐] 9월 20일

---
+++ 9월 14일 - 우선순위 큐/11286.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+/* 내림차순 cmp */
+struct cmp {
+	bool operator()(int n1, int n2) {
+		if (abs(n1) != abs(n2)) return abs(n1) > abs(n2);
+		return n1 > n2;
+	}
+};
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, num, min;
+	priority_queue<int, vector<int>,cmp> q;
+	cin >> n;
+
+	while (n--) {
+		cin >> num;
+		if (num == 0) {
+			if (q.empty()) min = 0;
+			else {
+				min = q.top();
+				q.pop();
+			}
+			cout << min << '\n';
+		}
+		else q.push(num);
+	}
+}
