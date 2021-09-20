ae22bdf9ab9ffe3a2154c99aaf39482d4254776c
thals1214 <thaxls@ewhain.net>
2021-09-20 11:55:09 +0900

    [우선순위큐] 9월 20일

---
+++ 9월 14일 - 우선순위 큐/15903.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	int n, m;
+	long long num, sum = 0; /* int: no! 문제 조건 잘보기 */
+	cin >> n >> m;
+
+	priority_queue<long long, vector<long long>, greater<>> pq;
+
+	for(int i = 0; i<n; i++) {
+		cin >> num;
+		pq.push(num);
+	}
+	while (m--) {
+		long long num1 = pq.top();
+		pq.pop();
+		long long num2 = pq.top();
+		pq.pop();
+		num1 += num2;
+		pq.push(num1);
+		pq.push(num1);
+	}
+	while(n--) {
+		sum += pq.top();
+		pq.pop();
+	}
+	cout << sum << '\n';
+}
+
