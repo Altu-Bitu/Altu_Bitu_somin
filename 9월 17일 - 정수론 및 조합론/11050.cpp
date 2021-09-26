805e3034cd7b1dcf7a3fc31447ab2367754a5ba6
thals1214 <thaxls@ewhain.net>
2021-09-26 17:27:38 +0900

    [정수론] 9월 26일 -ing

---
+++ 9월 17일 - 정수론 및 조합론/11050.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int fact(int n) {
+	if (n == 0) return 1; // 0 이면 1 (반복문 탈출 조건) 
+	return n * fact(n - 1); // 재귀 조건 
+}
+
+int main() {
+	int n, k, res; // 입력을 위한 변수 & 결과를 저장하기 위한 변수 
+	cin >> n >> k; // 입력 받기
+
+	res = fact(n) / (fact(k) * fact(n - k)); // 연산 -> n! / k! * (n-k)! 
+	cout << res<< '\n'; // 결과 출력 
+}
