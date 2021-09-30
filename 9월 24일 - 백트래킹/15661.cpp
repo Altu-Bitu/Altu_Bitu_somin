4916a5e9207f18a0137c67b449aaa81e00d9bf86
thals1214 <thaxls@ewhain.net>
2021-09-30 23:32:26 +0900

    [백트래킹] 9월 30일 -ing
    해결 x

---
+++ 9월 24일 - 백트래킹/15661.cpp
@@ -0,0 +1,19 @@
+﻿// 링크와 스타트 
+#include <iostream>
+
+using namespace std;
+
+const int SIZE = 20;
+int team[SIZE + 1][SIZE + 1];
+bool check[SIZE + 1];
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> team[i][j];
+		}
+	}
+}
