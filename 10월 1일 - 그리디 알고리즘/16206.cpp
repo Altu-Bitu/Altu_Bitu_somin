6d56294f36895187e706a6aade6eec1f1ad0afa2
thals1214 <thaxls@ewhain.net>
2021-10-06 21:04:14 +0900

    [그리디 알고리즘] 10월 1일 -update

--- 10월 1일 - 그리디 알고리즘/16206.cpp
+++ 10월 1일 - 그리디 알고리즘/16206.cpp
@@ -1,15 +1,14 @@
 ﻿//롤케이크 먹고싶다 
-//틀림
 
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
+int ans = 0;
 
-int ans = 0, cnt = 0;
-
-int roll_cake(int m, vector<int>& cake) {
+int roll_cake(deque<int>& cake, int m) {
+	int cnt = 0;
 	for (int i = 0; i < cake.size(); i++) {
 		while (cake[i] >= 10) {
 			if (cake[i] == 10) { // 길이가 10이라면
@@ -28,39 +27,33 @@ int roll_cake(int m, vector<int>& cake) {
 
 /* 
 * 10의 배수가 들어올 때 -> 10의 배수끼리도 정렬 해줘야 함! ( 10 , 20, 30, 40 예시) 
+* 10의 배수가 아닐 때 -> 굳이 정렬할 필요 없음 (ex 12 23) -> 함수 하나로 컨테이너 활용 
+* 
+* 10으로 자르는 부분 -> 수학적으로 한번에 해결 가능
 */
 
 int main() {
-	int n, m, temp; 
+	int n, m, temp, cnt = 0; 
 	
-
 	//입력
 	cin >> n >> m;
-	vector<int> cake_1;
-	vector<int> cake_0;
+	deque<int> cake;
+	//cake.assign(n, 0);
 
 	while (n--) {
 		cin >> temp;
-		if(temp % 10 != 0) cake_1.push_back(temp); //10의 배수 아닌 length
-		else { //10의 배수
-			if (temp == 10) {
-				ans++;
-				continue;
-			}
-			cake_0.push_back(temp); //10의 배수 모아 놓기 	
+		if (temp % 10 == 0) {
+			cake.push_front(temp);
+			cnt++;
 		}
+		else 
+			cake.push_back(temp);
+		
 	}
 
 	//연산 & 출력
-	sort(cake_0.begin(), cake_0.end());
-	sort(cake_1.begin(), cake_1.end()); 
-	
-	roll_cake(m, cake_0);
-	if (cnt == m) {
-		cout << ans;
-		return 0;
-	}
+	sort(cake.begin(), cake.begin()+cnt);
+	cout << roll_cake(cake, m);
 
-	cout << roll_cake(m, cake_1);
 	return 0;
 }
