dd8a55ecbc40a76e77c2cf9f9becbd4f6a3d6ef8
thals1214 <thaxls@ewhain.net>
2021-10-06 18:31:26 +0900

    [그리디 알고리즘] 10월 1일 -ing

---
+++ 10월 1일 - 그리디 알고리즘/1448.cpp
@@ -0,0 +1,28 @@
+﻿//삼각형 만들기
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int make_triangle(vector<int>& straws) {
+	int ans = 0;
+	for (int i = straws.size() - 1; i >= 2; i--) {
+		if (straws[i] < (straws[i - 1] + straws[i - 2])) return straws[i] + straws[i - 1] + straws[i - 2];
+	}
+	return -1;
+}
+int main() {
+	int n;
+	vector<int> straws;
+
+	//입력
+	cin >> n;
+	straws.assign(n, 0);
+	for (int i = 0; i < n; i++) 
+		cin >> straws[i];
+	
+	//연산
+	sort(straws.begin(), straws.end());
+	cout << make_triangle(straws);
+}
