7754b7205cd5baa363c48ac55166b01e84756598
thals1214 <thaxls@ewhain.net>
2021-10-06 18:31:19 +0900

    [그리디 알고리즘] 10월 1일 -ing

---
+++ 10월 1일 - 그리디 알고리즘/13305.cpp
@@ -0,0 +1,37 @@
+﻿//주유소
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<int> cities, road;
+
+long long min_cost(int n) {
+	long long min_cost = cities[0]; //initialize
+	long long cost = 0; //비용
+	for (int i = 0; i < n-1; i++) {
+		if (cities[i] < min_cost) min_cost = cities[i];
+		cost += min_cost * road[i];
+	}
+	return cost;
+}
+
+int main() {
+	int n; //도시의 수
+	
+	//입력
+	cin >> n;
+	
+	cities.assign(n, 0);
+	road.assign(n - 1, 0);
+
+	for (int i = 0; i < n-1; i++) {
+		cin >> road[i];
+	}
+	for (int i = 0; i < n; i++) {
+		cin >> cities[i];
+	}
+
+	//연산 & 출력
+	cout << min_cost(n) <<'\n';
+
+}
