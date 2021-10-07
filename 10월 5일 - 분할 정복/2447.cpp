336085105c26d72f74bf2100bb5a6c010fad8862
thals1214 <thaxls@ewhain.net>
2021-10-07 23:27:26 +0900

    [분할 정복] 10월 5일 -ing

---
+++ 10월 5일 - 분할 정복/2447.cpp
@@ -0,0 +1,32 @@
+﻿//별찍기
+#include <iostream>
+
+using namespace std;
+
+void draw_star(int i, int j, int n) {
+	if ((i / n) % 3 == 1 && (j / n) % 3 == 1) {
+		cout << " ";
+	}
+	else {
+		if (n == 1)  cout << "*"; //if(n / 3 ==0) 써도 됨
+		else draw_star(i, j, n / 3);
+	}
+}
+
+
+int main(){
+	int n;
+	
+	//입력
+	cin >> n;
+
+	//연산 & 출력
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			draw_star(i, j, n);
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
