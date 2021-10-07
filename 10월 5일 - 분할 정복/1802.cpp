9a4447fa7b43d317e9ba0ed56b049b6b1ace4f75
thals1214 <thaxls@ewhain.net>
2021-10-07 23:21:49 +0900

    [분할 정복] 10월 5일 -ing

---
+++ 10월 5일 - 분할 정복/1802.cpp
@@ -0,0 +1,31 @@
+﻿//종이접기
+#include <iostream>
+
+using namespace std;
+string s;
+
+string orgami(int size) {
+	if(size == 1) return "YES"; //기저 조건
+
+	int mid = size / 2;
+	for (int i = 1; i <= mid; i++) {
+		if (s[mid - i] == s[mid + i]) return "NO";
+	}
+	return orgami(mid);
+}
+
+int main() {
+	int t;
+
+	//입력 
+	cin >> t;
+
+	//연산 & 출력
+	while (t--) {
+		cin >> s;
+		int size = s.size();
+		cout << orgami(size)<<'\n';
+	}
+
+	return 0;
+}
