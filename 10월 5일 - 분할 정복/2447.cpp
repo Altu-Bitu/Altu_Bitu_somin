637a7b8e82308980d313893455f7f405fbd66ee7
thals1214 <thaxls@ewhain.net>
2021-10-08 11:00:54 +0900

    [분할 정복] 10월 5일 -ing

--- 10월 5일 - 분할 정복/2447.cpp
+++ 10월 5일 - 분할 정복/2447.cpp
@@ -4,15 +4,23 @@
 using namespace std;
 
 void draw_star(int i, int j, int n) {
+	//빈칸일 조건 
 	if ((i / n) % 3 == 1 && (j / n) % 3 == 1) {
 		cout << " ";
 	}
 	else {
-		if (n == 1)  cout << "*"; //if(n / 3 ==0) 써도 됨
+		if (n == 1)  cout << "*"; //별찍기 (n/3 == 0)도 가능 
 		else draw_star(i, j, n / 3);
 	}
 }
 
+/*
+* 빈 칸일 조건 
+* ex) 3*3배열 i,j -> (1,1)
+* ex) 9*9배열 i,j -> (1.1) (1.4) (1.7) 
+*			  i,j ->  3~ 5로 이루어진 모든 경우의수 
+*			  i,j -> (7,1) (7,4) (7,5) 
+*/
 
 int main(){
 	int n;
