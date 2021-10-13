0d6b087b378df5a06679cf297de383766157c593
thals1214 <thaxls@ewhain.net>
2021-10-13 21:43:50 +0900

    [이분 탐색] 10월 8일 - update

--- 1008 - Binary Search/3079.cpp
+++ 1008 - Binary Search/3079.cpp
@@ -7,48 +7,56 @@
 #include <algorithm>
 
 using namespace std;
-long long n, m, cnt, ans = 0; //n과 m도 longlong 이여야함!
+int n, m;
+long long cnt, ans = 0;
 
-long long minTime(long long max, vector<int> arr){
-    long long left = 0, right = max; //초기화 (0, 제일 오래 걸릴때 시간)
+long long minTime(long long left, long long max, vector<long long> arr) {
+    long long right = max; //초기화 (제일 오래 걸릴때 시간)
     ans = max; //ans 초기화
 
-    while(left <= right){
+    while (left <= right) {
         cnt = 0;
-        long long mid = (left + right)/2;
-        if(mid == 0) break;
+        long long mid = (left + right) / 2;
+        if (mid == 0) break;
 
-        for(long long i = 0; i<arr.size(); i++){
+        for (int i = 0; i < arr.size(); i++) {
             cnt += mid / arr[i];
         }
-        if(cnt >= m){
+        if (cnt >= m) {
             ans = mid;
             right = mid - 1;
         }
-        else{
+        else {
             left = mid + 1;
         }
     }
     return ans;
 }
 
-int main(){
+/*
+ *  long long인 mid와 int인 arr[i]를 나누면 오버플로우가 발생할 수 있음
+ *   -> arr를 long long으로 수정해줘야!
+ */
+
+int main() {
     //입출력 속도 향상
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
     //입력
-    cin >> n>> m;
-    vector<int> arr(n,0); //한 심사대당 걸리는 시간 배열
-    for(long long i = 0; i<n ; i++)
+    cin >> n >> m;
+    vector<long long> arr(n, 0); //한 심사대당 걸리는 시간 배열
+    for (int i = 0; i < n; i++)
         cin >> arr[i];
 
     //연산
-    sort(arr.begin(),arr.end()); //정렬 (오름차순)
+    sort(arr.begin(), arr.end()); //정렬 (오름차순)
+
+    long long left = arr.front(); //제일 최소로 걸리는 시간
     long long max = arr.back() * m; //제일 오래 걸릴 때 시간
 
     //출력
-    cout << minTime(max, arr);
+    cout << minTime(left ,max, arr);
 
     return 0;
 }
